d1ae31d7158dcfb918945563df674925541bcd88
G2Martins <gustavogripaldi@outlook.com>
2023-09-25 20:48:07 -0300

    first commit

---
+++ src/main/java/dio/springboot/aulas_spring/AulasSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot.aulas_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/springboot/aulas_spring/AulasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot.aulas_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
