ae961c0205ca53e654738e42050b0259610d087c
G2Martins <gustavogripaldi@outlook.com>
2023-09-25 23:42:17 -0300

    docs: add Aulas_SingletonPrototype.java files

---
+++ aulas_singleton_prototype/src/test/java/dio/springboot/aulas_singleton_prototype/AulasSingletonPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot.aulas_singleton_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasSingletonPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
