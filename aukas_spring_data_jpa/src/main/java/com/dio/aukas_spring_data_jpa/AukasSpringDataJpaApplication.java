b97b9ee9691048e0c571c07bc137987a11289a08
G2Martins <gustavogripaldi@outlook.com>
2023-09-26 01:25:33 -0300

    docs: aulas Spring Data

---
+++ aukas_spring_data_jpa/src/main/java/com/dio/aukas_spring_data_jpa/AukasSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.aukas_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AukasSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AukasSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ aukas_spring_data_jpa/src/test/java/com/dio/aukas_spring_data_jpa/AukasSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.aukas_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AukasSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
