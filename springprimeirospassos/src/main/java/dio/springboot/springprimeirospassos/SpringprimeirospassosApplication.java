341b19ee3343422303b488aff041a582da6ef71e
G2Martins <gustavogripaldi@outlook.com>
2023-09-26 00:33:43 -0300

    docs: Configuration Properties java files

---
+++ springprimeirospassos/src/main/java/dio/springboot/springprimeirospassos/SpringprimeirospassosApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot.springprimeirospassos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprimeirospassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprimeirospassosApplication.class, args);
+	}
+
+}
